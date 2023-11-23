2549d84fbc4c139d859ccfece553b0b1853ab986
Sanjit Sarkar <sanjit.est14@gmail.com>
2023-11-23 13:55:03 +0600

    Adding User Interface

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,32 @@
+import { Schema, model, connect } from 'mongoose'
+
+export type User = {
+  userId: string
+  username: string
+  password: number
+  fullName: {
+    firstName: string
+    lastName: string
+  }
+  age: number
+  email: string
+  isActive: true
+  hobbies: 'reading' | 'traveling'
+  address: {
+    street: string
+    city: string
+    country: string
+  }
+  orders: [
+    {
+      productName: string
+      price: number
+      quantity: number
+    },
+    {
+      productName: string
+      price: number
+      quantity: number
+    },
+  ]
+}
